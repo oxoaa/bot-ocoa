4970bbb98c37434ec5dcd6d8854c2a2de6551bd0
oxooa <oxooa@users.noreply.github.com>
2026-08-09 08:51:17 +0800

    chore: 移除用户业务数据，只保留框架核心

--- 插件/102352380/发文件.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if ($this->用户信息 == '发文件') {
-    $this->发送('文件', 'https://v11-weba.douyinvod.com/9fc063382dc7d809855280515ffa3f10/6a19ec2f/video/tos/cn/tos-cn-ve-15c000-ce/oot1IXBBMvQZaihBSEbolAkAaEPi4XoLKiaD4/?a=6383&ch=26&cr=3&dr=0&lr=all&cd=0%7C0%7C0%7C3&cv=1&br=2782&bt=2782&cs=0&ds=4&ft=rVWEerwwZRcZseFo1PDS6kwc3RZtGm2_IU9eF89RLyD12nzXT&mime_type=video_mp4&qs=0&rc=OmY4ODU4ZGVmZjw0O2czaEBpM3E3OXI5cm9wOzMzbGkzNUAwXjJfNTFjNWMxMjAxMmE1YSNmM2RwMmRrZWBhLS1kLTVzcw%3D%3D&btag=c0000e00008000&cquery=100B_100x_100z_100o_100w&dy_q=1780072947&feature_id=37f92ebd2877ae8e7eba995d406c5150&l=20260530004227B859C52F06750E24893B');
-}
